c3c5a976564affef4b7e6bacddc90823da688c8b
Fishdestroyer <jeffwhitner@gmail.com>
2022-03-10 11:21:44 -0600

    Added code for github and linked in

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import githubicon from "../../assets/githubicon.jpg";
 import linkedinicon from "../../assets/linkedinicon.jpg";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 
 function Social() {
 
   return (
     <section className="my-5" >
-      <h1 id="social">Find me on GitHub</h1>
-      <img src={githubicon}  className="githubicon"  style={{ width: "25%" }} alt="github" />
+      <h1 id="social">Find me on GitHub and Linkedin </h1>
+      <ul>
+          <li><a href="https://github.com/Fishdestroyer" img src={githubicon}>GitHub</a></li>
+          <li><a href="https://www.linkedin.com">Linkedin</a></li>
+      </ul>
+      <img src={githubicon}  className="githubicon"   style={{ width: "25%" }} alt="github" />
+      <img src={linkedinicon}  className="linkedinicon"   style={{ width: "25%" }} alt="linkedin" />
       
-      <div className="my-5">
-          <h1 id="social">Find me on linkedin</h1>
-          <img src={linkedinicon}  className="linkedinicon" Link="https://github.com/Fishdestroyer" style={{ width: "25%" }} alt="github" />
-      </div>
+      
+      
+      
     </section>
   )
 }

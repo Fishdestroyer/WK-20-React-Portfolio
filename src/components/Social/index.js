082328b56039226aff8557d4dea398d34a904a3c
Fishdestroyer <jeffwhitner@gmail.com>
2022-03-10 10:27:33 -0600

    added linked in info

--- src/components/Social/index.js
+++ src/components/Social/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import coverImage from "../../assets/0.jpg";
+import githubicon from "../../assets/githubicon.jpg";
+import linkedinicon from "../../assets/linkedinicon.jpg";
+//import { Link } from "react-router-dom";
 
 function Social() {
 
   return (
     <section className="my-5" >
       <h1 id="social">Find me on GitHub</h1>
-      <img src={coverImage} className="my-2" to="https://github.com/account" style={{ width: "25%" }} alt="cover" />
+      <img src={githubicon}  className="githubicon"  style={{ width: "25%" }} alt="github" />
+      
       <div className="my-2">
-       
+          <h1 id="social">Find me on linkedin</h1>
+          <img src={linkedinicon}  className="linkedinicon"  style={{ width: "25%" }} alt="github" />
       </div>
     </section>
   )
